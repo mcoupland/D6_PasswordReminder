2266805165f562d57af093e19449cb7fba2b07c0
Michael Coupland <couplandihc@gmail.com>
2018-02-08 11:47:56 -0600

    Add files via upload

---
+++ form-passwordreminder.tpl.php
@@ -0,0 +1,13 @@
+<div class="passwordreminder-content">
+	<p class="passwordreminder-header">Forgot your username and/or password?</p>
+	<p class="passwordreminder-header">Simply provide the email address attached to your Andrew Harper membership below, and we'll send it to this address.</p>
+	<div>
+		<label>Email Address:</label><?php print drupal_render($email); ?>
+	</div>
+	<div class="passwordreminder-normal">
+		If you have not set up a user name and password, please click here. If you have any difficulty, please email Membership@AndrewHarper.com or call (866) 831-4314 from the U.S or +1 (512) 904-7342 internationally.
+	</div>
+</div>
+<div>
+	<?php print drupal_render($submitbutton); ?>
+</div>
